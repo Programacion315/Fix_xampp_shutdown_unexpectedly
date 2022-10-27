db38fec936ee64af14589cba9c87b01aafafceaa
Programacion315 <jorgeluishuergo88@gmail.com>
2022-10-27 08:10:10 -0500

    copy folder

--- configuration.py
+++ configuration.py
@@ -1,2 +1,4 @@
 path_data = "C:\\Users\Luis\\Desktop\\Prueba\\data"
 path_old_data = "C:\\Users\Luis\\Desktop\\Prueba\\data_old"
+backup = "C:\\Users\Luis\\Desktop\\Prueba\\backup"
+ibdata1 = "C:\\Users\Luis\\Desktop\\Prueba\\data_old\\ibdata1"
